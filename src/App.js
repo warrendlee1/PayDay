a926aebd53f667a1ebe57da190cc8cb15b9f07d5
minaghanna <minaghanna@gmail.com>
2020-02-01 18:47:44 -0800

    Add google authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import Form from './Form';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
+import Form from "./pages/Form";
+
+//pages
+import MainPage from "./pages";
+import NotFoundPage from "./pages/404";
+import UserPage from "./pages/users";
+// import Form from "./Users/minaghanna/Desktop/PayDay/src/pages/Form.js"
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainPage}></Route>
+          <Route exact path="/404" component={NotFoundPage}></Route> 
+          <Route exact path="/users" component={UserPage}></Route>
+          <Route exact path="/Form" component={Form}></Route>
+          <Redirect to="/404"/>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
 export default App;
---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+    return(
+        <div>
+            <h2> 404 Not Found </h2>
+            <h3> It looks like you're trying to access a page that doesn't exist! </h3>
+        </div>
+    )
+}
+
+export default NotFoundPage
